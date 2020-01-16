f6b553e5772e115c93e2032df98ce75c3b0f7174
157350 <aliabbas.riaz@almarai.com>
2020-01-16 09:50:16 +0300

    Improved comment Added

--- app/src/main/java/aliabbas/com/codebaseapp/di_dependencies/app_dependencies/MainActivityRefMod.kt
+++ app/src/main/java/aliabbas/com/codebaseapp/di_dependencies/app_dependencies/MainActivityRefMod.kt
@@ -7,11 +7,14 @@ import dagger.Module
 
 /*
  * Created by Ali Abbas
- * This module is used to tell the DI about the Network helpers we have and the object we need to inject.
+ * This module is used to inject activity references
  *
  */
 @Module
 abstract class MainActivityRefMod {
+
+    //With the help of this We can Inject the activity in Dependent classes
+    //Making activity reference available for injection.
     @Binds
-    abstract fun homeActivityRefrence(homeActivity: MainActivity?): Activity?
+    abstract fun homeActivityRefrence(homeActivity: MainActivity): Activity
 }
