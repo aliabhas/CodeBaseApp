d3be5ae4870c17931fd8c4a56fe66989c9360500
157350 <aliabbas.riaz@almarai.com>
2020-07-15 16:47:30 +0300

    Hello test new branch

--- app/src/main/java/aliabbas/com/codebaseapp/ui_screens/fragments/pixabay_scrren/adapter/PixaBaySearchAdapter.kt
+++ app/src/main/java/aliabbas/com/codebaseapp/ui_screens/fragments/pixabay_scrren/adapter/PixaBaySearchAdapter.kt
@@ -19,6 +19,7 @@ import javax.inject.Inject
  * This class hold the detail to display the recyclerview data
  * Hello Version 1.34
  * Hello test new branch
+ * Doing changes in master, thanks
  */
 class PixaBaySearchAdapter @Inject constructor() :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
